16a11a234a3af056e83ae9f3e16ced2dd425ef62
Artyte <artyte@gmail.com>
2016-12-31 01:34:58 +0800

    fixed the voice

--- app/src/main/java/com/example/artyte/pracnum/MainActivity.java
+++ app/src/main/java/com/example/artyte/pracnum/MainActivity.java
@@ -24,33 +24,33 @@ public class MainActivity extends Activity {
     protected void listen(View v) {
         tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             public void onInit(int status) {
-                if (status != TextToSpeech.ERROR) {
+                if (status == TextToSpeech.SUCCESS) {
                     tts.setLanguage(Locale.JAPAN);
+
+                    t1 = (TextView) findViewById(R.id.text);
+                    String toSpeak = t1.getText().toString();
+                    tts.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null, "speak");
                 }
             }
         });
-
-
-        t1 = (TextView) findViewById(R.id.text);
-        String toSpeak = t1.getText().toString();
-        tts.speak("hello", TextToSpeech.QUEUE_ADD, null, "speak");
     }
 
     protected void newNum(View v) {
-        int min = 0;
-        int max = 100000000;
-        int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
         tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             public void onInit(int status) {
-                if (status != TextToSpeech.ERROR) {
+                if (status == TextToSpeech.SUCCESS) {
                     tts.setLanguage(Locale.JAPAN);
+
+                    int min = 0;
+                    int max = 100000000;
+                    int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
+
+                    t1 = (TextView) findViewById(R.id.text);
+                    t1.setText(Integer.toString(randomNum));
+                    tts.speak(Integer.toString(randomNum), TextToSpeech.QUEUE_FLUSH, null, "speak");
                 }
             }
         });
-
-        t1 = (TextView) findViewById(R.id.text);
-        t1.setText(Integer.toString(randomNum));
-        tts.speak(Integer.toString(randomNum), TextToSpeech.QUEUE_ADD, null, "speak");
     }
 
     public void onPause() {
